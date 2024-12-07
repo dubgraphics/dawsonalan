c89500c455682fd870db28b815847c4feda47691
Dawson Williams <dawsonwilliams@Mac.attlocal.net>
2024-12-06 18:23:04 -0600

    Deploy static files

--- obj/Release/net9.0/PortfolioSite.AssemblyInfo.cs
+++ obj/Release/net9.0/PortfolioSite.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioSite")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01dd8670d2ca7b7882a9fc10573b361c185d7db")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioSite")]
